c2c1214a7dd49a0e89b8e54794450337776779b0
Not Me <notmebutme5544@gmail.com>
2025-09-01 19:44:32 +0000

    remove the fade in thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,34 +7,19 @@ import { Founder } from "@/components/founder";
 import { Quotes } from "@/components/quotes";
 import { Booking } from "@/components/booking";
 import { Footer } from "@/components/footer";
-import { FadeIn } from "@/components/ui/fade-in";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
-        <FadeIn>
-          <Hero />
-        </FadeIn>
-        <FadeIn>
-          <Services />
-        </FadeIn>
-        <FadeIn>
-          <Process />
-        </FadeIn>
-        <FadeIn>
-          <Comparison />
-        </FadeIn>
-        <FadeIn>
-          <Founder />
-        </FadeIn>
-        <FadeIn>
-          <Quotes />
-        </FadeIn>
-        <FadeIn>
-          <Booking />
-        </FadeIn>
+        <Hero />
+        <Services />
+        <Process />
+        <Comparison />
+        <Founder />
+        <Quotes />
+        <Booking />
       </main>
       <Footer />
     </div>
